78c0f9040ef1003ecceaaab633601579a5ee20a4
YogitaTayde <yogitatayde9@gmail.com>
2025-10-12 15:53:24 +0530

    Implemented ArrayList

---
+++ ArrayList/src/Student.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+	public static void main(String[] args) {
+		
+		
+	
+	List l=new ArrayList();
+	l.add("yogita");
+	l.add("tayde");
+	l.add("1234");
+	l.add("tayde"); 
+ System.out.println(l);
+	}
+}
+
